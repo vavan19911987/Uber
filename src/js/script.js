3221a56129f07cd64204cd2b8da8543d67a58e94
vavan19911987 <v.pulin@yahoo.com>
2021-06-01 03:18:39 +0200

    сделал  favicon и отправление формы

--- src/js/script.js
+++ src/js/script.js
@@ -61,5 +61,28 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
+    $('form').submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+          }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize(),
+        }).done(function () {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+        
+
+    });
+
+
 
 });
